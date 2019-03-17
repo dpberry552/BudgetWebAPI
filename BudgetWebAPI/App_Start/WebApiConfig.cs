410dd5fd338c2d57cad07903eaa62de806086641
dpberry552 <dpberry178@gmail.com>
2019-03-16 20:30:45 -0400

    modify config to return JSON by default

--- BudgetWebAPI/App_Start/WebApiConfig.cs
+++ BudgetWebAPI/App_Start/WebApiConfig.cs
@@ -10,7 +10,14 @@ namespace BudgetWebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+            .Add(new System.Net.Http.Formatting.RequestHeaderMapping
+            (   "Accept",
+                "text/html",
+                StringComparison.InvariantCultureIgnoreCase,
+                true,
+                "application/json")
+            );
             // Web API routes
             config.MapHttpAttributeRoutes();
 
