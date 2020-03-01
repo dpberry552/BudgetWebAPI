f3b73b21dae4efa962354bb8bf7f386ec10f8a6e
dpberry552 <dpberry178@gmail.com>
2020-02-29 21:39:48 -0500

    project updates, enable CORS

--- BudgetWebAPI/App_Start/WebApiConfig.cs
+++ BudgetWebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BudgetWebAPI
 {
@@ -26,6 +27,9 @@ namespace BudgetWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "GET,POST");
+            config.EnableCors(cors);
         }
     }
 }
