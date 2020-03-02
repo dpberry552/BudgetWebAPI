f70a0844411063527d4b698c6c7f293b5f6c2178
dpberry552 <dpberry178@gmail.com>
2020-03-01 22:29:30 -0500

    adding Transaction controller

---
+++ BudgetWebAPI/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BudgetDataAccess.Models;
+using BudgetDataAccess.Connections;
+
+namespace BudgetWebAPI.Controllers
+{
+    public class TransactionController : ApiController
+    {
+        [Route("api/transaction")]
+        public IEnumerable<BusinessObject<Transaction>> Get()
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return Transaction.GetAll(db);
+            }
+        }
+    }
+}
